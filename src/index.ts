576c284d6bf76240b4a04d6dc24c273c03bcf6dd
Marcos J. Peña Pollastri <marcos.p.pollastri@gmail.com>
2023-05-25 20:28:40 -0300

    patch: added version to the main script and jsonresolver

--- src/index.ts
+++ src/index.ts
@@ -2,28 +2,30 @@
 import * as moduleAlias from 'module-alias';
 moduleAlias.addAlias('@src', __dirname);
 import { Command } from 'commander';
+import * as packageJson from '../package.json';
 import * as fs from 'fs-extra';
 import * as path from 'node:path';
 
-const program = new Command();
+export async function run(argv: string[]) {
+  const program = new Command();
 
-program
-  .version('0.0.1')
-  .command('new <project-name>')
-  .description('Create a new project')
-  .action(async (projectName: string) => {
-    try {
+  program
+    .version(packageJson.version)
+    .command('new <project-name>')
+    .description('Create a new project')
+    .action(async (projectName: string) => {
       console.log(`Creating a new project: ${projectName}`);
       const templateDir = path.resolve(__dirname, '../template/simply-express-ts');
       const targetDir = path.resolve(process.cwd(), projectName);
 
       await fs.copy(templateDir, targetDir);
       console.log('Project created!');
-    } catch (error) {
-      console.error('Error while creating your project: ', error.message);
-      throw error
-    }
-    
-  });
+    });
 
-program.parse(process.argv);
+  await program.parseAsync(argv);
+}
+
+if (require.main === module) {
+  run(process.argv);
+}
+
---
+++ tests/Index.test.ts
@@ -0,0 +1,50 @@
+import { Command } from 'commander';
+import * as fs from 'fs-extra';
+import * as path from 'node:path';
+import { run } from '../src/index';
+
+jest.mock('fs-extra');
+jest.mock('path');
+jest.mock('commander', () => {
+  return { Command: jest.fn() };
+});
+
+describe.skip('Test the command line application', () => {
+  const copyMock = jest.mocked(fs.copy);
+  const resolveMock = jest.mocked(path.resolve);
+  // const MockedCommand = jest.createMockFromModule(typeof Command) as Command;
+  
+  beforeEach(() => {
+    jest.clearAllMocks()
+  });
+
+  it('Should create a new project', async () => {
+    // const projectName = 'test-project';
+
+    // resolveMock.mockImplementation((...args) => {
+    //   return args.join('/');
+    // });
+
+    // copyMock.mockImplementation(() => Promise.resolve());
+
+    // const commandInstance = MockedCommand
+    // commandInstance.version = jest.fn().mockReturnThis();
+    // commandInstance.command = jest.fn().mockReturnThis();
+    // commandInstance.description = jest.fn().mockReturnThis();
+    // commandInstance.action = jest.fn().mockImplementation((cb) => {
+    //   cb(projectName);
+    //   return commandInstance;
+    // });
+    
+    // // Here we simulate process.argv values
+    // await run(['node', 'test', 'new', projectName]);
+
+    // expect(commandInstance.version).toHaveBeenCalledWith(expect.any(String));
+    // expect(commandInstance.command).toHaveBeenCalledWith('new <project-name>');
+    // expect(commandInstance.description).toHaveBeenCalledWith('Create a new project');
+    // expect(commandInstance.action).toHaveBeenCalledWith(expect.any(Function));
+    // expect(resolveMock).toHaveBeenCalledWith(expect.any(String), '../template/simply-express-ts');
+    // expect(resolveMock).toHaveBeenCalledWith(process.cwd(), projectName);
+    // expect(copyMock).toHaveBeenCalledWith(expect.any(String), expect.any(String));
+  });
+});
